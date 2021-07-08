8bb39f312dc4db527d3b2f221e6c30cf3f6130f3
MmeOokamy <manue.ggt@gmail.com>
2021-07-08 22:18:18 +0200

    good night before refactorig :)

--- Diamonback_explorer/main.py
+++ Diamonback_explorer/main.py
@@ -1,8 +1,14 @@
+from kivy import platform
+from kivy.config import Config
+Config.set('graphics', 'width', '900')
+Config.set('graphics', 'height', '400')
 from kivy.app import App
+from kivy.core.window import Window
 from kivy.graphics.context_instructions import Color
 from kivy.graphics.vertex_instructions import Line
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, Clock
 from kivy.uix.widget import Widget
+
 
 
 class MainWidget(Widget):
@@ -10,12 +16,19 @@ class MainWidget(Widget):
     perspective_point_y = NumericProperty(0)
 
     V_NB_LINES = 10
-    V_LINES_SPACING = .2  # 10% in screen width
+    V_LINES_SPACING = .25  # 25% in screen width
     vertical_lines = []
 
-    H_NB_LINES = 15
-    H_LINES_SPACING = .2  # 10% in screen width
+    H_NB_LINES = 8
+    H_LINES_SPACING = .15  # 10% in screen width
     horizontal_lines = []
+
+    SPEED = 4
+    current_offset_y = 0
+
+    SPEED_X = 12
+    current_speed_x = 0
+    current_offset_x = 0
 
     def __init__(self, **kwargs):
         super(MainWidget, self).__init__(**kwargs)
@@ -23,12 +36,27 @@ class MainWidget(Widget):
         self.init_vertical_lines()
         self.init_horizontal_lines()
 
+        if self.is_desktop():
+            self._keyboard = Window.request_keyboard(self.keyboard_closed, self)
+            self._keyboard.bind(on_key_down=self.on_keyboard_down)
+            self._keyboard.bind(on_key_up=self.on_keyboard_up)
+            Clock.schedule_interval(self.update, 1 / 60)
+
+    def keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self.on_keyboard_down)
+        self._keyboard = None
+
+    def is_desktop(self):
+        if platform in ('linux', 'win', 'macosx'):
+            return True
+        return False
+
     def on_parent(self, widget, parent):
         print("On Parent W: " + str(self.width) + " H: " + str(self.height))
 
     def on_size(self, *args):
-        self.update_vertical_lines()
-        self.update_horizontal_lines()
+        # self.update_vertical_lines()
+        # self.update_horizontal_lines()
         # I don't need this function I declare it in the diamonback.kv
         pass
         # print("On Size W: " + str(self.width) + " H: " + str(self.height))
@@ -59,7 +87,7 @@ class MainWidget(Widget):
         spacing = self.V_LINES_SPACING * self.width
         offset = -int(self.V_NB_LINES / 2) + 0.5
         for i in range(0, self.V_NB_LINES):
-            lines_x = int(central_line_x + offset * spacing)
+            lines_x = int(central_line_x + offset * spacing + self.current_offset_x)
             x1, y1 = self.transform(lines_x, 0)
             x2, y2 = self.transform(lines_x, self.height)
             self.vertical_lines[i].points = [x1, y1, x2, y2]
@@ -74,18 +102,19 @@ class MainWidget(Widget):
             for i in range(0, self.H_NB_LINES):
                 self.horizontal_lines.append(Line())
 
+    # calculates the points to display the line
     def update_horizontal_lines(self):
         central_line_x = self.width / 2
         spacing = self.V_LINES_SPACING * self.width
         offset = -int(self.V_NB_LINES / 2) + 0.5
 
-        min_x = central_line_x + offset * spacing
-        max_y = central_line_x - offset * spacing
+        min_x = central_line_x + offset * spacing + self.current_offset_x
+        max_x = central_line_x - offset * spacing + self.current_offset_x
         spacing_y = self.H_LINES_SPACING * self.height
         for i in range(0, self.H_NB_LINES):
-            lines_y = int(i * spacing_y)
+            lines_y = i * spacing_y - self.current_offset_y
             x1, y1 = self.transform(min_x, lines_y)
-            x2, y2 = self.transform(max_y, lines_y)
+            x2, y2 = self.transform(max_x, lines_y)
             self.horizontal_lines[i].points = [x1, y1, x2, y2]
 
     # intermediate function to change the behaviour of the display without changing all the code
@@ -98,15 +127,54 @@ class MainWidget(Widget):
 
     def transform_perspective(self, x, y):
         # tr_y: allows the height to be set at the perspective point
-        tr_y = y * self.perspective_point_y / self.height
-        if tr_y > self.perspective_point_y:
-            tr_y = self.perspective_point_y
+        # and we change for linear => lin_y
+        lin_y = y * self.perspective_point_y / self.height
+        if lin_y > self.perspective_point_y:
+            lin_y = self.perspective_point_y
 
         diff_x = x - self.perspective_point_x
-        diff_y = self.perspective_point_y - tr_y
-        offset_x = diff_x * diff_y / self.perspective_point_y
+        diff_y = self.perspective_point_y - lin_y
+        factor_y = diff_y / self.perspective_point_y
+        factor_y = pow(factor_y, 4)  # like factor_y * factor_y if 2,
+
+        offset_x = diff_x * factor_y
+
         tr_x = self.perspective_point_x + offset_x
+        tr_y = self.perspective_point_y - factor_y * self.perspective_point_y
         return int(tr_x), int(tr_y)
+
+    def on_touch_down(self, touch):
+        if touch.x < self.width / 2:
+            self.current_speed_x = self.SPEED_X
+        else:
+            self.current_speed_x = -self.SPEED_X
+
+    def on_touch_up(self, touch):
+        self.current_speed_x = 0
+
+    def on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'left':
+            self.current_speed_x = self.SPEED_X
+        elif keycode[1] == 'right':
+            self.current_speed_x = -self.SPEED_X
+        return True
+
+    def on_keyboard_up(self, keyboard, keycode):
+        self.current_speed_x = 0
+
+    def update(self, dt):
+        # print("dt: " + str(dt*60))
+        time_factor = dt * 60
+        self.update_vertical_lines()
+        self.update_horizontal_lines()
+        self.current_offset_y += self.SPEED_X * time_factor
+
+        spacing_y = self.H_LINES_SPACING * self.height
+        if self.current_offset_y >= spacing_y:
+            self.current_offset_y -= spacing_y
+
+        self.current_offset_x += self.current_speed_x * time_factor
+        # self.current_offset_x -= self.SPEED_X * time_factor
 
 
 class DiamonbackApp(App):
